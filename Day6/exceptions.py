0256130fef0c14437a6ec9a924b88d147df21a0c
venkata <venkata.penneru@delta.com>
2025-01-30 19:19:57 +0530

    Added Exceptions

---
+++ Day6/exceptions.py
@@ -0,0 +1,81 @@
+# The string printed as the exception type is the name of the built-in exception that occurred. This is true for all built-in exceptions, but need not be true for user-defined exceptions (although it is a useful convention).
+# Standard exception names are built-in identifiers (not reserved keywords)
+# The rest of the line provides detail based on the type of exception and what caused it.
+
+# which asks the user for input until a valid integer has been entered,
+# but allows the user to interrupt the program (using Control-C or whatever the operating system supports);
+while True:
+    try:
+        number = int(input("Enter a valid number:"))
+        # The number will only be printed if its a valid value else the print statement will be skipped and
+        # except block code is executed
+        print(number)
+        break
+    except ValueError as e:
+        print("Oops thats a wrong type please enter an integer")
+
+# The unhandled exceptions will result in error check for below code
+# while True:
+#     try:
+#         number = int(input("Enter a valid number:"))
+#         # The number will only be printed if its a valid value else the print statement will be skipped and
+#         # except block code is executed
+#         print(number)
+#         break
+#     # Here we are not handling VAlueerror which will Stop the code execution
+#     except TypeError as e:
+#         print("Oops thats a wrong type lease enter an integer")
+#
+
+## Multiple except statements
+while True:
+    try:
+        number = int(input("Enter a valid number for the multiple exceptions code:"))
+        # The number will only be printed if its a valid value else the print statement will be skipped and
+        # except block code is executed
+        print(number)
+        break
+    # Here we are not handling VAlueerror which will Stop the code execution
+    except TypeError as e:
+        print(e)
+    except ValueError as e:
+        print("Oops thats a wrong type lease enter an integer")
+
+
+# Exception Inheritance Code
+class B(Exception):
+    pass
+
+
+class C(B):
+    pass
+
+
+class D(C):
+    pass
+
+
+# Here the exceptions are raised based upon the matches
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
+# Reversing the exception order the matching exception will be raised first ! Note C and D are derived from the B
+# The below try catch will always raise an exception related to B as it is the parent class of C and D
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except B:
+        print("B")
+    except C:
+        print("C")
+    except D:
+        print("D")
+
+
+# Making custom exceptions
