dd52044d90838cf7b4860451326de0f6d44c7cef
heirl <phonexid65@gmail.com>
2025-02-08 20:09:30 +0530

    added code for generator example

--- Day7/generators.py
+++ Day7/generators.py
@@ -16,12 +16,12 @@ def generate_a_sublist(max):
     max_range = 100
     while count < max:
         # Generate a sublist of 100 numbers starting from `count` to `count + 100`
-        new_list = [x for x in range(count, min(count + 100, max))]
-        yield new_list
+        new_list = [x for x in range(count, min(count + 100, max)) if x % 100 == 0]
+        yield len(new_list)
         count += 100  # Move the starting point by 100 for the next iteration
 
 # Example usage
-alist = generate_a_sublist(10000)
+alist = generate_a_sublist(100000000)
 for a_list in alist:
     #time.sleep(10)
     print(a_list)
@@ -29,6 +29,8 @@ for a_list in alist:
 
 #Same Function without a generator
 def generate_a_list(max):
-    new_list = [x for x in range(max) if x%1000000 == 0]
+    # Generate a list with numbers from 0 to `max` that are divisible by 1,000,000
+    new_list = [x for x in range(max) if x % 100 == 0]
     return len(new_list)
+
 print(generate_a_list(100000000))
